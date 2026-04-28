0f177ed8904f5b8d173ab0560b15e2c5b7d1742d
MojtabaTajik <onyfel@gmail.com>
2026-04-28 22:49:23 +0200

    Move scan to background thread, add cancel support
    ScanHijack and ScanImportMethods ran on the main thread, freezing the
    UI during large directory scans. Replaced with TScanThread which runs
    all PE parsing, signature checks, and method collection off the main
    thread. Results are posted back via TThread.Synchronize so the TreeView
    updates progressively as each file completes.
    
    Filter options are captured into TScanOptions at scan start so the
    thread never touches VCL controls. The Scan button toggles to Cancel
    while a scan is active. FormClose terminates any running thread.
    
    Also eliminates the two-pass scan design: methods are now collected in
    the same pass as hijack detection, removing the second TreeView walk.

---
+++ Robber/ScanThread.pas
@@ -0,0 +1,260 @@
+unit ScanThread;
+
+interface
+
+uses
+  System.Classes, System.SysUtils, System.IOUtils, System.Types,
+  System.Generics.Collections,
+  DLLHijack, DigitalSignature;
+
+type
+  TDLLScanInfo = record
+    Name: string;
+    Methods: TArray<string>;
+  end;
+
+  TScanResult = record
+    ExePath: string;
+    FileSize: Cardinal;
+    IsX86: Boolean;
+    IsSigned: Boolean;
+    SignerCompany: string;
+    HijackRate: THijackRate;
+    DLLs: TArray<TDLLScanInfo>;
+  end;
+
+  TScanOptions = record
+    SearchPath: string;
+    ImageTypeFilter: Integer;
+    SignFilter: Integer;
+    AbuseCandidateFilter: Integer;
+    WritePermFilter: Integer;
+    BestChoiceDLLCount: Integer;
+    BestChoiceExeSize: Integer;
+    GoodChoiceDLLCount: Integer;
+    GoodChoiceExeSize: Integer;
+  end;
+
+  TOnScanProgress = procedure(Current, Total: Integer; const FileName: string) of object;
+  TOnScanResult = procedure(const Result: TScanResult) of object;
+  TOnScanDone = procedure(Cancelled: Boolean) of object;
+
+  TScanThread = class(TThread)
+  private
+    FOptions: TScanOptions;
+    FOnProgress: TOnScanProgress;
+    FOnResult: TOnScanResult;
+    FOnDone: TOnScanDone;
+
+    // Staging fields for Synchronize callbacks (no params allowed)
+    FSyncCurrent: Integer;
+    FSyncTotal: Integer;
+    FSyncFileName: string;
+    FSyncResult: TScanResult;
+    FSyncCancelled: Boolean;
+
+    procedure DoProgress;
+    procedure DoResult;
+    procedure DoDone;
+
+    function FilterHijackRate(Rate: THijackRate): Boolean;
+    function FilterImageType(IsX86: Boolean): Boolean;
+    function FilterSign(IsSigned: Boolean): Boolean;
+    function FilterWritePerm(const FilePath: string): Boolean;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(const Options: TScanOptions;
+      OnProgress: TOnScanProgress;
+      OnResult: TOnScanResult;
+      OnDone: TOnScanDone);
+  end;
+
+implementation
+
+constructor TScanThread.Create(const Options: TScanOptions;
+  OnProgress: TOnScanProgress; OnResult: TOnScanResult; OnDone: TOnScanDone);
+begin
+  inherited Create(False);
+  FreeOnTerminate := True;
+  FOptions := Options;
+  FOnProgress := OnProgress;
+  FOnResult := OnResult;
+  FOnDone := OnDone;
+end;
+
+procedure TScanThread.DoProgress;
+begin
+  if Assigned(FOnProgress) then
+    FOnProgress(FSyncCurrent, FSyncTotal, FSyncFileName);
+end;
+
+procedure TScanThread.DoResult;
+begin
+  if Assigned(FOnResult) then
+    FOnResult(FSyncResult);
+end;
+
+procedure TScanThread.DoDone;
+begin
+  if Assigned(FOnDone) then
+    FOnDone(FSyncCancelled);
+end;
+
+function TScanThread.FilterHijackRate(Rate: THijackRate): Boolean;
+begin
+  case FOptions.AbuseCandidateFilter of
+    0: Result := False;
+    1: Result := Rate <> hrBest;
+    2: Result := Rate <> hrGood;
+    3: Result := Rate <> hrBad;
+  else
+    Result := False;
+  end;
+end;
+
+function TScanThread.FilterImageType(IsX86: Boolean): Boolean;
+begin
+  case FOptions.ImageTypeFilter of
+    0: Result := False;
+    1: Result := not IsX86;  // x86 only: skip x64
+    2: Result := IsX86;      // x64 only: skip x86
+  else
+    Result := False;
+  end;
+end;
+
+function TScanThread.FilterSign(IsSigned: Boolean): Boolean;
+begin
+  case FOptions.SignFilter of
+    0: Result := False;
+    1: Result := not IsSigned;  // signed only: skip unsigned
+  else
+    Result := False;
+  end;
+end;
+
+function TScanThread.FilterWritePerm(const FilePath: string): Boolean;
+const
+  TempFileName = 'RobberWriteCheck.txt';
+var
+  TempFilePath: string;
+  FS: TFileStream;
+begin
+  if FOptions.WritePermFilter = 0 then
+    Exit(False);
+
+  // Return True = skip (dir is NOT writable = not exploitable)
+  // Return False = keep (dir IS writable = hijackable)
+  TempFilePath := TPath.Combine(TPath.GetDirectoryName(FilePath), TempFileName);
+  try
+    FS := TFile.Create(TempFilePath);
+    try
+      Result := False;
+    finally
+      FS.Free;
+      TFile.Delete(TempFilePath);
+    end;
+  except
+    Result := True;
+  end;
+end;
+
+procedure TScanThread.Execute;
+var
+  FileList: TStringDynArray;
+  EachFile, DLLName: string;
+  PEFile: TDLLHijack;
+  Signature: TDigitalSignature;
+  ImportDLLs, Methods: TStringList;
+  IsSigned: Boolean;
+  HijackRate: THijackRate;
+  Res: TScanResult;
+  DLLInfo: TDLLScanInfo;
+  DLLInfoList: TList<TDLLScanInfo>;
+  i: Integer;
+begin
+  FileList := TDirectory.GetFiles(FOptions.SearchPath, '*.exe',
+    TSearchOption.soAllDirectories);
+
+  FSyncTotal := Length(FileList);
+  FSyncCurrent := 0;
+  FSyncCancelled := False;
+
+  for EachFile in FileList do
+  begin
+    if Terminated then
+    begin
+      FSyncCancelled := True;
+      Break;
+    end;
+
+    Inc(FSyncCurrent);
+    FSyncFileName := EachFile;
+    Synchronize(DoProgress);
+
+    try
+      ImportDLLs := TStringList.Create;
+      PEFile := TDLLHijack.Create(EachFile);
+      Signature := TDigitalSignature.Create(EachFile);
+      try
+        PEFile.GetHijackableImportedDLL(ImportDLLs);
+        if ImportDLLs.Count = 0 then
+          Continue;
+
+        HijackRate := PEFile.GetHijackRate(
+          FOptions.BestChoiceDLLCount, FOptions.BestChoiceExeSize,
+          FOptions.GoodChoiceDLLCount, FOptions.GoodChoiceExeSize);
+
+        if FilterHijackRate(HijackRate) then Continue;
+        if FilterImageType(PEFile.IsX86Image) then Continue;
+
+        IsSigned := Signature.IsCodeSigned;
+        if FilterSign(IsSigned) then Continue;
+        if FilterWritePerm(EachFile) then Continue;
+
+        // Build result record
+        Res.ExePath := EachFile;
+        Res.FileSize := PEFile.GetFileSize;
+        Res.IsX86 := PEFile.IsX86Image;
+        Res.IsSigned := IsSigned;
+        Res.SignerCompany := Signature.SignerCompany;
+        Res.HijackRate := HijackRate;
+
+        // Collect DLLs and their methods in one pass
+        Methods := TStringList.Create;
+        DLLInfoList := TList<TDLLScanInfo>.Create;
+        try
+          for DLLName in ImportDLLs do
+          begin
+            Methods.Clear;
+            PEFile.GetDLLMethods(DLLName, Methods);
+            DLLInfo.Name := DLLName;
+            SetLength(DLLInfo.Methods, Methods.Count);
+            for i := 0 to Methods.Count - 1 do
+              DLLInfo.Methods[i] := Methods[i];
+            DLLInfoList.Add(DLLInfo);
+          end;
+          Res.DLLs := DLLInfoList.ToArray;
+        finally
+          Methods.Free;
+          DLLInfoList.Free;
+        end;
+
+        FSyncResult := Res;
+        Synchronize(DoResult);
+
+      finally
+        Signature.Free;
+        ImportDLLs.Free;
+        PEFile.Free;
+      end;
+    except
+      // Skip files we can't read (access denied, corrupt PE, etc.)
+    end;
+  end;
+
+  Synchronize(DoDone);
+end;
+
+end.
